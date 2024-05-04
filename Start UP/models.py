c00652f63da84e16de2b2f91b21d7853738da660
Amanali Karatay <160749839+JackSparrow322432@users.noreply.github.com>
2024-05-04 15:08:13 +0600

    Delete Start UP directory

--- Start UP/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UserProfile(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-
-    def _str_(self):
-        return self.username
